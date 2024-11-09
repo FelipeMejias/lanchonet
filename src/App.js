5b0143234b3f744dab5d94486a8bdae614de0e07
Felipe Mejias <felipe.mejias50@gmail.com>
2024-11-09 17:38:42 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Item from './Item';
+import { getComidas } from './api';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export function App(){
+    const [itens,setItens]=useState([])
+    const listaDeComidas= itens.map(formarComponente)
+    function formarComponente(item){
+        return <Item nome={item.nome} preco={item.preco}/>
+    }
+    function buscarComidas(){
+      const promessa=getComidas()
+      promessa.then(alterarIntens)
+    }
+    function alterarIntens(res){
+        setItens(res.data)
+    }
+    useEffect(buscarComidas,[])
+    return(
+        <Container>
+          {listaDeComidas}
+        </Container>
+    )
+  }
+  const Container=styled.div`
+  display:flex;flex-direction:column;align-items:center;
+  height:100vh;width:100vw;background-color:green;
+  overflow:auto;
+  `
---
+++ src/Item.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+export default function Item({nome,descricao,preco}){
+    return(
+        <Conteudo color={preco>20}>
+            <h1>{nome}</h1>
+            <h2>{preco},00</h2>
+        </Conteudo>
+    )
+}
+const Conteudo=styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:space-evenly;
+width:200px;
+height:100px;
+background-color:white;
+margin-top:30px;
+border-radius:15px;
+h1{font-size:22px;font-weight:500;margin:0}
+h2{color:green;font-size:16px;margin:0}
+`
